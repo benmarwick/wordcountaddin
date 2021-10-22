37d154a062e0f12ca48489a74f6bb1f4a8d7eb54
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2021-10-22 11:41:39 +0100

    Add new functionality to improve handling of HTML tags

--- R/hello.R
+++ R/hello.R
@@ -181,8 +181,26 @@ prep_text <- function(text){
   # don't include # for headings
   text <- gsub("#*", "", text)
 
-  # don't include html tags
-  text <- gsub("<.+?>|</.+?>", "", text)
+  # don't include opening html tags
+  # (source: https://www.w3schools.com/TAGS/default.ASP)
+
+  tags <- paste0("!DOCTYPE|a|abbr|acronym|address|applet|area|article|aside|",
+                 "audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|",
+                 "canvas|caption|center|cite|code|col|colgroup|data|datalist|",
+                 "dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|",
+                 "figcaption|figure|font|footer|form|frame|frameset|h1 to h6|",
+                 "head|header|hr|html|i|iframe|img|input|ins|kbd|label|legend|",
+                 "li|link|main|map|mark|meta|meter|nav|noframes|noscript|",
+                 "object|ol|optgroup|option|output|p|param|picture|pre|",
+                 "progress|q|rp|rt|ruby|s|samp|script|section|select|small|",
+                 "source|span|strike|strong|style|sub|summary|sup|svg|table|",
+                 "tbody|td|template|textarea|tfoot|th|thead|time|title|tr|",
+                 "track|tt|u|ul|var|video|wbr")
+
+  text <- gsub(paste0("<\\s*(",tags,")[^>]*>"),"", text)
+
+  # don't include closing html tags
+  text <- gsub("</.+?>", "", text)
 
   # don't include percent signs because they trip up stringi
   text <- gsub("%", "", text)
